5fcd5b7c725d064560690cd5281027137e44ad42
VinayShanigarapu <103049341+VinayShanigarapu@users.noreply.github.com>
2023-10-05 22:12:15 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from V-Bot'
+  })
+})
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 1, // Higher values means the model will take more risks.
+      max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    });
+    console.log(response.data);
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
